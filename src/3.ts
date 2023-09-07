801e1d695a4fa211da28870201bd44663daf6a8e
OlexTS <sashokseven@gmail.com>
2023-09-07 20:07:39 +0300

    finished 4th task

--- src/3.ts
+++ src/3.ts
@@ -8,31 +8,31 @@
 */
 
 interface ICharacter {
-  name: string,
-  level: number,
+  name: string;
+  level: number;
 
-  introduce(phrase: string): void,
-  levelUp(): void
+  introduce(phrase: string): void;
+  levelUp(): void;
 }
 
 interface ISpellCaster {
-  castSpell(): void
+  castSpell(): void;
 }
 
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
-      throw new Error('Level too low');
+      throw new Error("Level too low");
     }
   }
 
   introduce(phrase: string): void {
-    console.log(phrase + ', ' + this.name);
+    console.log(phrase + ", " + this.name);
   }
 
   castSpell(): void {
-    console.log('Casting a spell, behold my power!');
+    console.log("Casting a spell, behold my power!");
   }
 
   levelUp(): void {
@@ -42,10 +42,10 @@ class Wizard implements ICharacter, ISpellCaster {
 }
 
 // тестування класу
-const wizard = new Wizard('Merlin', 15);
+const wizard = new Wizard("Merlin", 15);
 
-wizard.introduce('I am the mighty wizard');
+wizard.introduce("I am the mighty wizard");
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp(); // Level up! New level is 16
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature: string;
+  constructor() {
+    this.signature = (Math.random() * 10).toString();
+  }
+  getSignature(): string {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (!this.door) {
+      return;
+    }
+    this.tenants.push(person);
+  }
+
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): boolean {
+    if (key.getSignature() === this.key.getSignature()) {
+      return (this.door = true);
+    }
+    return (this.door = false);
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
